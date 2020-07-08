acabbd24fc5ac36841163fa9ab75e4328942d84c
Raghu412 <matrixbender.412@gmail.com>
2020-07-08 17:14:01 +0530

    Updates

---
+++ precache-manifest.546c5b3c81e38e3274a9e01c2fef0fe1.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7809dab8cb1d8cad6d62606e4178bef2",
+    "url": "/projects/index.html"
+  },
+  {
+    "revision": "0b8a857672ab755c18e6",
+    "url": "/projects/static/css/main.e129e96a.chunk.css"
+  },
+  {
+    "revision": "b66ad59933a2554ca212",
+    "url": "/projects/static/js/2.4fd0455b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/projects/static/js/2.4fd0455b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b8a857672ab755c18e6",
+    "url": "/projects/static/js/main.052bf178.chunk.js"
+  },
+  {
+    "revision": "f639c188cb940516bcf1",
+    "url": "/projects/static/js/runtime-main.da0864a9.js"
+  },
+  {
+    "revision": "5df09c9978393454b2193be7f874b8ee",
+    "url": "/projects/static/media/1.5df09c99.png"
+  },
+  {
+    "revision": "b6f5d7119a784e94e77e614444031683",
+    "url": "/projects/static/media/1.b6f5d711.png"
+  },
+  {
+    "revision": "c5c376432ab3407c27c88b28d2e1cec7",
+    "url": "/projects/static/media/1.c5c37643.png"
+  },
+  {
+    "revision": "7d8dc86522f744ad13db9ba68db63b01",
+    "url": "/projects/static/media/10.7d8dc865.png"
+  },
+  {
+    "revision": "dfe76952aefc6162355216a148adadfd",
+    "url": "/projects/static/media/10.dfe76952.png"
+  },
+  {
+    "revision": "20c77dadefc3b47694b33ba880236e85",
+    "url": "/projects/static/media/11.20c77dad.png"
+  },
+  {
+    "revision": "7f5d62d4571237d897a8c5609e15a85a",
+    "url": "/projects/static/media/11.7f5d62d4.png"
+  },
+  {
+    "revision": "56a9a493cf700fa57c578437a2d5272e",
+    "url": "/projects/static/media/12.56a9a493.png"
+  },
+  {
+    "revision": "411c8613380a3a1b017e2a1d5811d1ab",
+    "url": "/projects/static/media/2.411c8613.png"
+  },
+  {
+    "revision": "6332d8b4883f3e858b402556882c738f",
+    "url": "/projects/static/media/2.6332d8b4.png"
+  },
+  {
+    "revision": "8f9bce3a916b7b677d5b74c17595c047",
+    "url": "/projects/static/media/2.8f9bce3a.png"
+  },
+  {
+    "revision": "6e16c2d9c5d0e8e341459fd02cb9ef67",
+    "url": "/projects/static/media/3.6e16c2d9.png"
+  },
+  {
+    "revision": "9355f2bc88d8178705de0f72db486815",
+    "url": "/projects/static/media/3.9355f2bc.png"
+  },
+  {
+    "revision": "95f52e327fac85a92ea4604e6fbd131d",
+    "url": "/projects/static/media/4.95f52e32.png"
+  },
+  {
+    "revision": "e5eb888c52830735272b42f1fd5d0dcf",
+    "url": "/projects/static/media/4.e5eb888c.png"
+  },
+  {
+    "revision": "4d1404a7b6594ab04688db454246f11f",
+    "url": "/projects/static/media/5.4d1404a7.png"
+  },
+  {
+    "revision": "8e8b79d3f2fdaa75d435ee8733cba3d0",
+    "url": "/projects/static/media/5.8e8b79d3.png"
+  },
+  {
+    "revision": "44b00ece0de1a3da6bd2e1cf3a7d1193",
+    "url": "/projects/static/media/6.44b00ece.png"
+  },
+  {
+    "revision": "e4d32771f6c32fb393e5f93366322e8f",
+    "url": "/projects/static/media/6.e4d32771.png"
+  },
+  {
+    "revision": "c6d6faec37d2ae3ce781f1fa96996f57",
+    "url": "/projects/static/media/7.c6d6faec.png"
+  },
+  {
+    "revision": "f794feb4e7045a2d200f08d19a7a8865",
+    "url": "/projects/static/media/7.f794feb4.png"
+  },
+  {
+    "revision": "2a93e82e6b984a3ed8beebdcd732e733",
+    "url": "/projects/static/media/8.2a93e82e.png"
+  },
+  {
+    "revision": "b497a1e4fb2fbf97bea943f9e55af85e",
+    "url": "/projects/static/media/8.b497a1e4.png"
+  },
+  {
+    "revision": "0e213c34b5297fcebf583e86e98b1935",
+    "url": "/projects/static/media/9.0e213c34.png"
+  },
+  {
+    "revision": "6cc7705955dd78dd00b1f054c6c65dfa",
+    "url": "/projects/static/media/9.6cc77059.png"
+  },
+  {
+    "revision": "5640c918874ff2debfaa4386fb1035fe",
+    "url": "/projects/static/media/errmsg.5640c918.png"
+  },
+  {
+    "revision": "771ecefe15adc2e057111bf4307fdc50",
+    "url": "/projects/static/media/video.771ecefe.mkv"
+  },
+  {
+    "revision": "93ffa47a3dcd18fb67062952ada9654c",
+    "url": "/projects/static/media/video.93ffa47a.mkv"
+  }
+]);
